54408b1330a0f838486c507b1d1691fd9a3487fe
Alena <alenaporfoevakorr@gmail.com>
2026-05-04 22:43:45 +0200

    feat: towelSort function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix) return [];
+
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    const row = matrix[i];
+
+    if (i % 2 === 0) {
+      result.push(...row);
+    } else {
+      result.push(...[...row].reverse());
+    }
+  }
+
+  return result;
 };
